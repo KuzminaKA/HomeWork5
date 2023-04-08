ad7496b7fc51642d402fe72662e4f5e1a8e6650d
KseniaKuzmina <ksenya.kuzmina83@gmail.com>
2023-04-08 17:38:27 +0300

    First task added

---
+++ Task1/Program.cs
@@ -0,0 +1,48 @@
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int Getint(string arg)
+{
+    System.Console.Write($"введите {arg}");
+    return int.Parse(Console.ReadLine());
+} 
+
+int [] Createarray(int Length)
+{
+    int [] array= new int[Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+
+void Printarray(int [] array)
+{
+    System.Console.Write("[ ");
+    for (int i = 0; i < array.Length-1; i++)
+    {
+        System.Console.Write(array[i] + "; ");
+    }
+    System.Console.Write(array[array.Length-1] + " ]");
+}
+
+void Countpositive(int [] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]%2==0)
+        {
+            count++;
+        }
+    }
+    System.Console.WriteLine(count);
+}
+
+int Length= Getint("длину массива: ");
+
+int [] array = Createarray(Length);
+Printarray(array);
+System.Console.WriteLine();
+Countpositive(array);
