932acabdc0924d9ddbf9ca8282d12fd511dca0da
KseniaKuzmina <ksenya.kuzmina83@gmail.com>
2023-04-08 17:39:50 +0300

    Second and third tasks added

---
+++ Task2/Program.cs
@@ -0,0 +1,49 @@
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+
+int Getint(string arg)
+{
+    System.Console.Write($"введите {arg}");
+    return int.Parse(Console.ReadLine());
+} 
+
+int [] Createarray(int Length, int max, int min)
+{
+    int [] array= new int[Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+    return array;
+}
+
+void Printarray(int [] array)
+{
+    System.Console.Write("[ ");
+    for (int i = 0; i < array.Length-1; i++)
+    {
+        System.Console.Write(array[i] + "; ");
+    }
+    System.Console.Write(array[array.Length-1] + " ]");
+}
+
+void SumOdd(int [] array)
+{
+   int Sum=0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i%2 != 0)
+        {
+            Sum = Sum + array[i];
+        }
+    }
+   System.Console.WriteLine("сумма элементов на нечетных позициях = " + Sum);
+}
+
+int Length= Getint("длину массива:");
+int max = Getint("максимальный элемент массива: ");
+int min = Getint ("минимальный элемент массива: ");
+int [] array = Createarray(Length, max, min);
+Printarray(array);
+System.Console.WriteLine();
+SumOdd(array);
---
+++ Task3/Program.cs
@@ -0,0 +1,51 @@
+﻿//Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+double [] array = {-1.25, 48.75, 11.73, -47.75};
+
+void Printarray(double [] array)
+{
+    System.Console.Write("[ ");
+    for (int i = 0; i < array.Length-1; i++)
+    {
+        System.Console.Write(array[i] + "; ");
+    }
+    System.Console.Write(array[array.Length-1] + " ]");
+}
+
+double FindMax(double [] array)
+{
+    double max=0;
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i]>max)
+    {
+        max=array[i];
+    }
+}
+return max;
+}
+
+double FindMin(double [] array)
+{
+    double min=0;
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i]<min)
+    {
+        min=array[i];
+    }
+}
+return min;
+}
+
+double Max=FindMax(array);
+double Min=FindMin(array);
+
+void FindDiff(double Max, double Min)
+{
+    double Diff=Max-Min;
+    System.Console.WriteLine("разность между макимальным и минимальным значением = " + Diff);
+}
+Printarray(array);
+System.Console.WriteLine();
+FindDiff(Max, Min);
